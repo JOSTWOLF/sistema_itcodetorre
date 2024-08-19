b15297818f6986ec450a545001be2c2d864c3c55
Jose Torre <144872784+JOSTWOLF@users.noreply.github.com>
2024-08-19 16:26:22 -0500

    Add files via upload

---
+++ SistemaITcodetorre/app/src/main/java/PaqueteConeccion/Conexion.java
@@ -0,0 +1,13 @@
+package PaqueteConeccion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ITCODE
+ */
+public class Conexion {
+
+}
